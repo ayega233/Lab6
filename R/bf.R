88c86ee53071dba19f1fb203b9b8544587759c5b
Ayesha Gamage <144423992+ayega233@users.noreply.github.com>
2023-10-12 22:03:00 +0200

    test functions

--- R/bf.R
+++ R/bf.R
@@ -2,18 +2,22 @@
 #' @import rlist
 
 
-#' @export knapsack_brute_force
+#' @export brute_force_knapsack
 #' @description
 #' knapsack_brute_force search, i.e. going through all possible alternatives and return the maximum value
 #'found. This approach is of complexity O(2n) since all possible combinations 2n needs to be evaluated
-#' @param x snapsack object as dataframe
-#' @param W snapsack size
+#' @param x knapsack object as dataframe
+#' @param W knapsack size
 #' @return result the list with maximum values and elements.
-knapsack_brute_force<-function(x, W){
+
+brute_force_knapsack<-function(x, W){
   v <-x$v
   w <-x$w
   n<-length(x$v)
+  #storage of all combination of knapsack
   all_combinations<-matrix(0,ncol = n)
+
+  #binary represenation of all combinations
   for (i in 1:2^n) {
     bits<-intToBits(i)
     combination<-c()
@@ -25,6 +29,7 @@ knapsack_brute_force<-function(x, W){
   }
   combination_weight <- list()
 
+  #calculate total weight and total values of all combinations
   for (i in 1:2^n){
     current_combination <- all_combinations[i,]
     current_weight <-0
@@ -39,10 +44,7 @@ knapsack_brute_force<-function(x, W){
     combination_weight <- append(combination_weight,item)
 
   }
-  #print(combination_weight)
 
-
-  #selected_index <- which(combination_weight ==(combination_weight<=W), arr.ind=TRUE)
   max_value<-0
   max_value_item <- NULL
   for (i in 1:length(combination_weight)){
@@ -56,11 +58,6 @@ knapsack_brute_force<-function(x, W){
       }
     }
   }
-  #list.filter(combination_weight, w <= W)
-
-  #print(combination_weight)
-  #max_values <-lapply(combination_weight,max)
-  #print(max_value_item)
 
   rtn_value<- list("value"= c(round(max_value_item$v)),"elements"=which(max_value_item$combination %in% c(1)))
   return(rtn_value)
--- R/ks.R
+++ R/ks.R
@@ -1,25 +1,3 @@
-##old sampler used for backward compatibility
-suppressWarnings(RNGversion(min(as.character(getRversion()),"3.5.3")))
-set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
-n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000)
-  )
-
-knapsack_objects2 <-
-  data.frame(
-    w=c(4,3,2,1),
-    v=c(5,4,3,2)
-  )
-
-knapsack_objects3 <-
-  data.frame(
-    w=c(1,2,3,4),
-    v=c(5,4,3,2)
-  )
-
 knapsack_dynamic<-function(x, W){
   v <-x$v
   w <-x$w
